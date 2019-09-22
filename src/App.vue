eade345b6b47bfc86eef20c94ed4acbad16d5c93
João Pedro Moraes <joaopedromoraes@outlook.com>
2019-09-21 23:10:56 -0300

    Adiciona novo formato de visualizacao

--- src/App.vue
+++ src/App.vue
@@ -8,51 +8,40 @@
       </nav>
     </div>
 
-    <div class="container">
-      <table class="highlight centered">
-        <thead>
-          <tr>
-            <th>Nº</th>
-            <th>Imagem</th>
-            <th>Pokémon</th>
-            <!-- <th>URL</th> -->
-            <th>Info</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="produto of produtos" :key=" produto.url.substring(34, produto.url.length - 1) ">
-            <td> {{ produto.url.substring(34, produto.url.length - 1) }}</td>
-            <td>
-              <img :src="imgPokemon(produto.url) " />
-            </td>
-            <td>{{ produto.name }}</td>
-            <!-- <td>{{ produto.url }}</td> -->
-            <td>
-              <a
-                class="btn-floating waves-effect waves-light blue modal-trigger"
-                href="#modal1"
-                v-on:click="infoPokemon(produto.url)"
-              >
-                <i class="material-icons">info_outline</i>
-              </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="row">
+      <div class="col s6 m4 l2" v-for="pokemon of pokemons">
+        <div class="card modal-trigger" href="#modal1" v-on:click="infoPokemon(pokemon.url)">
+          <div class="card-image">
+            <img :src="imgPokemon(pokemon.url) " />
+            <a class="btn-floating halfway-fab waves-effect waves-light red">
+              <i class="material-icons">add</i>
+            </a>
+          </div>
+          <div class="card-content grey">
+            <span
+              class="card-title center-align white-text"
+            >{{ capitalizeFirstLetter(pokemon.name) }}</span>
+          </div>
+        </div>
+      </div>
     </div>
     <br />
 
-    <div class="container center-align">
-      <a
-        class="waves-effect waves-light btn blue"
-        v-if="previous!==null"
-        v-on:click="listar(previous);"
-      >
-        <i class="material-icons left">chevron_left</i>anterior
-      </a>
-      <a class="waves-effect waves-light btn blue" v-if="next!==null" v-on:click="listar(next);">
-        <i class="material-icons right">chevron_right</i>proximo
-      </a>
+    <div class="row">
+      <div class="col s6 center">
+        <a
+          class="waves-effect waves-light btn-large blue"
+          v-if="previous!==null"
+          v-on:click="listar(previous);"
+        >
+          <i class="material-icons left">chevron_left</i>anterior
+        </a>
+      </div>
+      <div class="col s6 center">
+        <a class="waves-effect waves-light btn-large blue" v-if="next!==null" v-on:click="listar(next);">
+          <i class="material-icons right">chevron_right</i>proximo
+        </a>
+      </div>
     </div>
     <br />
 
@@ -67,7 +56,6 @@
         <ul id="example-1">
           <li v-for="habilidade in habilidades">- {{ habilidade.ability.name }}</li>
         </ul>
-
       </div>
       <div class="modal-footer red">
         <a href="#!" class="modal-close waves-effect waves-green btn-flat white-text">Fechar</a>
@@ -83,7 +71,7 @@ import Pokemon from "./services/pokeapi";
 export default {
   data() {
     return {
-      produtos: [],
+      pokemons: [],
       next: null,
       previous: null,
 
@@ -104,7 +92,7 @@ export default {
     listar(next) {
       Pokemon.listar(next).then(resposta => {
         // console.table(resposta.data.results);
-        this.produtos = resposta.data.results;
+        this.pokemons = resposta.data.results;
         this.next = resposta.data.next;
         this.previous = resposta.data.previous;
         // console.log(this.next);
@@ -131,6 +119,12 @@ export default {
         url.substring(34, url.length - 1) +
         ".png"
       );
+    },
+
+    capitalizeFirstLetter(string) {
+      return string.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      });
     }
   }
 };
