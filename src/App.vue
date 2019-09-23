4d07f1864d27b63eb6ec581258ce4383bf81819d
João Pedro Moraes <coticproeg@gmail.com>
2019-09-23 10:44:06 -0300

    Reformulação do carregamento dos pokemons

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <!-- ========Inicio da Barra de navegação======== -->
     <div class="navbar-fixed">
       <nav>
         <div class="nav-wrapper red darken-1">
@@ -7,9 +8,11 @@
         </div>
       </nav>
     </div>
+    <!-- ========Fim da Barra de navegação======== -->
 
     <div class="row">
-      <div class="col s12 m6 l4 xl2" v-for="pokemons of listaDepokemons">
+      <!-- ========Inicio Card Pokémons======== -->
+      <div class="col s12 m6 l4 xl2" v-for="pokemons of listaDepokemons" v-bind:key="pokemons.id">
         <div class="card modal-trigger" href="#modal1" v-on:click="infoPokemon(pokemons.name)">
           <!-- <div class="card waves-effect waves-block waves-light"> -->
           <div>
@@ -32,51 +35,63 @@
           </div>
         </div>
       </div>
-    </div>
-    <br />
+      <!-- ========Fim Card Pokémons======== -->
 
-    <div class="row">
-      <div class="col s6 center">
-        <a
-          class="waves-effect waves-light btn-large blue"
-          v-if="previous!==null"
-          v-on:click="listar(previous);"
-        >
-          <i class="material-icons left">chevron_left</i>anterior
-        </a>
+      <!-- ========Inicio Botão de navegação======== -->
+      <div class="col s12 m6 l4 xl2" v-on:click="nextPokemons();">
+        <div class="card modal-trigger">
+          <!-- <div class="card waves-effect waves-block waves-light"> -->
+          <div>
+            <div class="card-image">
+              <img src="default.png" title="Botão de proximo" alt="Imagem frontal do Pokémon" />
+              <a class="btn-floating halfway-fab waves-effect waves-light red">
+                <i class="material-icons">add</i>
+              </a>
+            </div>
+            <div class="card-content blue">
+              <span class="card-title center-align white-text activator">Mais</span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="col s6 center">
-        <a
-          class="waves-effect waves-light btn-large blue"
-          v-if="next!==null"
-          v-on:click="listar(next);"
-        >
-          <i class="material-icons right">chevron_right</i>proximo
-        </a>
-      </div>
+      <!-- ========Fim Botão de navegação======== -->
     </div>
-    <br />
 
-    <!-- Modal Structure -->
-    <div id="modal1" class="modal modal-fixed-footer" :class="pokemon.cor" >
+    <!-- ========Inicio da Estrutura do Modal======== -->
+    <div id="modal1" class="modal modal-fixed-footer" :class="pokemon.cor">
       <div class="modal-content">
         <img class="responsive-img" :src="pokemon.foto" />
         <h4 class="center-align">{{ pokemon.nome }} {{ pokemon.id }}#</h4>
-        <p><b>Peso:</b> {{ pokemon.peso }} kg</p>
-        <p><b>Altura:</b> {{ pokemon.altura }} m</p>
-        <p><b>Habilidades:</b></p>
+        <p>
+          <b>Peso:</b>
+          {{ pokemon.peso }} kg
+        </p>
+        <p>
+          <b>Altura:</b>
+          {{ pokemon.altura }} m
+        </p>
+        <p>
+          <b>Habilidades:</b>
+        </p>
         <p v-for="habilidade in pokemon.habilidades">- {{ habilidade.ability.name }}</p>
-        <p><b>Tipo:</b></p>
+        <p>
+          <b>Tipo:</b>
+        </p>
         <p v-for="tipo in pokemon.tipo">- {{ tipo.type.name }}</p>
-        <p><b>Eggs Group:</b></p>
+        <p>
+          <b>Eggs Group:</b>
+        </p>
         <p v-for="eggs in pokemon.grupo_ovo">- {{ eggs.name }}</p>
-        <p><b>Movimentos:</b></p>
+        <p>
+          <b>Movimentos:</b>
+        </p>
         <p v-for="movimentos in pokemon.movimentos">- {{ movimentos.move.name }}</p>
       </div>
       <div class="modal-footer black">
         <a href="#!" class="modal-close waves-effect waves-green btn-flat white-text">Fechar</a>
       </div>
     </div>
+    <!-- =====Fim da Estrutura do Modal======== -->
   </div>
 </template>
 
@@ -87,9 +102,9 @@ import PokemonApi from "./services/pokeapi";
 export default {
   data() {
     return {
+      listaGeralPokemon: [],
       listaDepokemons: [],
-      next: null,
-      previous: null,
+      next: 20,
       pokemon: {
         id: null,
         habilidades: [],
@@ -113,18 +128,20 @@ export default {
     listar(next) {
       PokemonApi.listar(next).then(resposta => {
         // console.table(resposta.data.results);
-        this.listaDepokemons = resposta.data.results;
-        this.next = resposta.data.next;
-        this.previous = resposta.data.previous;
+        this.listaGeralPokemon = resposta.data.results;
+        this.listaDepokemons = this.listaGeralPokemon.slice(0, 20);
+        // this.next = resposta.data.next;
+        // this.previous = resposta.data.previous;
         // console.log(this.next);
         // console.log(this.previous);
       });
     },
+    
     infoPokemon(name) {
       PokemonApi.pokemonGeral(name).then(resposta => {
         this.pokemon.habilidades = resposta.data.abilities;
         this.pokemon.foto = resposta.data.sprites.front_default;
-        this.pokemon.nome = resposta.data.name;
+        this.pokemon.nome = this.capitalizeFirstLetter(resposta.data.name);
         this.pokemon.id = resposta.data.id;
         this.pokemon.peso = resposta.data.weight / 10;
         this.pokemon.altura = resposta.data.height / 10;
@@ -134,11 +151,10 @@ export default {
         // console.log(this.foto);
         // console.log(this.nome);
       }),
-      PokemonApi.especie(name).then(resposta =>{
-        this.pokemon.grupo_ovo = resposta.data.egg_groups;
-        this.pokemon.cor = resposta.data.color.name;
-        console.log(this.pokemon.cor);
-      });
+        PokemonApi.especie(name).then(resposta => {
+          this.pokemon.grupo_ovo = resposta.data.egg_groups;
+          this.pokemon.cor = resposta.data.color.name;
+        });
     },
 
     imgPokemon(url) {
@@ -153,6 +169,13 @@ export default {
       return string.replace(/\w\S*/g, function(txt) {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
       });
+    },
+
+    nextPokemons() {
+      this.listaDepokemons.push(
+        ...this.listaGeralPokemon.slice(this.next, this.next + 20)
+      );
+      this.next = this.next + 20;
     }
   }
 };
--- src/services/pokeapi.js
+++ src/services/pokeapi.js
@@ -2,9 +2,13 @@ import axios from 'axios'
 
 export default {
 
-    listar:(next = 'https://pokeapi.co/api/v2/pokemon/') => {
+    listar:(next = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=10090') => {
         return axios.get(next)
     },
+
+    // listar:(next = 'https://pokeapi.co/api/v2/pokemon/') => {
+    //     return axios.get(next)
+    // },
 
     pokemonGeral:(name) =>{
         return axios.get('https://pokeapi.co/api/v2/pokemon/'+name)
